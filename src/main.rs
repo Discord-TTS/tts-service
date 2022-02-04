e5ad2ee2a2be7c4a8339f15f349ed0a150da78db
Gnome <david2005thomas@gmail.com>
2022-02-04 10:11:56 +0000

    Generate a new IP per retry

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,12 @@ async fn get_tts(
         .append_pair("textlen", &text.len().to_string())
         .finish();
 
-    let client = reqwest::Client::builder()
-        .local_address(Some(get_random_ipv6()))
-        .timeout(std::time::Duration::from_secs(2))
-        .build()?;
-
     for _ in 0..5 {
+        let client = reqwest::Client::builder()
+            .local_address(Some(get_random_ipv6()))
+            .timeout(std::time::Duration::from_secs(2))
+            .build()?;
+
         match client.get(url.clone()).send().await {
             Ok(mut resp) => {
                 resp = resp.error_for_status()?;
